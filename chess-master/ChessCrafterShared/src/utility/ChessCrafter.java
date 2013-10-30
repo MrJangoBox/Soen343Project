cc0f2dfd06f74d54e58cfc28fde5d0084aedae2c
MR Jango <d3t0ur@hotmail.com>
2013-10-30 13:54:31 -0400

    first commit

---
+++ chess-master/ChessCrafterShared/src/utility/ChessCrafter.java
@@ -0,0 +1,27 @@
+package utility;
+
+import gui.PlayGameScreen;
+import gui.PlayNetGameScreen;
+import gui.WatchGameScreen;
+import java.io.File;
+import logic.Game;
+
+public interface ChessCrafter
+{
+
+	public PlayGameScreen getPlayGameScreen(Game game);
+
+	public PlayNetGameScreen getNetGameScreen();
+
+	public PlayNetGameScreen getNetGameScreen(Game g, boolean isPlayback, boolean isBlack);
+
+	public void setFileMenuVisibility(boolean visibility);
+
+	public void setOptionsMenuVisibility(boolean visibility);
+
+	public void revertToMainPanel();
+
+	public void setPanel(Object newPanel);
+
+	public WatchGameScreen getWatchGameScreen(File acnFile);
+}
